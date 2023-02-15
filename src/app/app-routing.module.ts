8dfd9331b64fae53ff0efeff8027ae3e2383614b
Muhammad Syafiq Nordin <muhammad.syafiq@apu.edu.my>
2023-02-15 18:01:21 +0800

    feat(app-routing): added product page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./pages/product/product.module').then( m => m.ProductPageModule)
   }
 ];
 @NgModule({
